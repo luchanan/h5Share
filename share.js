9774f37b55094c162dbbfc83a511fb4165020cc8
“yluchanan <398778994@qq.com>
2016-10-18 10:26:52 +0800

    微信分享回调BUG

--- share.js
+++ share.js
@@ -171,14 +171,14 @@
                     imgUrl:_this.settings.pic, // 分享图标
                     success: function () {
                         // 用户确认分享后执行的回调函数
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareSucess();
+                        if($.isFunction(_this.settings.wechatShareSucess)){
+                            _this.settings.wechatShareSucess();
                         }
                     },
                     cancel: function (){
                         // 用户取消分享后执行的回调函数
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareCancel();
+                        if($.isFunction(_this.settings.wechatShareCancel)){
+                            _this.settings.wechatShareCancel();
                         }
                     }
                 });
@@ -191,13 +191,13 @@
                     type: 'link', // 分享类型,music、video或link，不填默认为link
                     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                     success: function () {
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareSucess();
+                        if($.isFunction(_this.settings.wechatShareSucess)){
+                            _this.settings.wechatShareSucess();
                         }
                     },
                     cancel: function () {
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareCancel();
+                        if($.isFunction(_this.settings.wechatShareCancel)){
+                            _this.settings.wechatShareCancel();
                         }
                     }
                 });
@@ -208,13 +208,13 @@
                     link: _this.settings.url, // 分享链接
                     imgUrl: _this.settings.pic, // 分享图标
                     success: function () {
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareSucess();
+                        if($.isFunction(_this.settings.wechatShareSucess)){
+                            _this.settings.wechatShareSucess();
                         }
                     },
                     cancel: function () {
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareCancel();
+                        if($.isFunction(_this.settings.wechatShareCancel)){
+                            _this.settings.wechatShareCancel();
                         }
                     }
                 });
@@ -225,13 +225,13 @@
                     link:_this.settings.url, // 分享链接
                     imgUrl:_this.settings.pic, // 分享图标
                     success: function () {
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareSucess();
+                        if($.isFunction(_this.settings.wechatShareSucess)){
+                            _this.settings.wechatShareSucess();
                         }
                     },
                     cancel: function () {
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareCancel();
+                        if($.isFunction(_this.settings.wechatShareCancel)){
+                            _this.settings.wechatShareCancel();
                         }
                     }
                 });
@@ -242,13 +242,13 @@
                     link: _this.settings.url, // 分享链接
                     imgUrl:_this.settings.pic, // 分享图标
                     success: function () {
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareSucess();
+                        if($.isFunction(_this.settings.wechatShareSucess)){
+                            _this.settings.wechatShareSucess();
                         }
                     },
                     cancel: function () {
-                        if($.isFunction(_this.wechatShareSucess)){
-                            _this.wechatShareCancel();
+                        if($.isFunction(_this.settings.wechatShareCancel)){
+                            _this.settings.wechatShareCancel();
                         }
                     }
                 });
